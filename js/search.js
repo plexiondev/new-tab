5663f002a7baad69887d82fbd0f54d68c47c5ae1
plexion <46572320+plexiondev@users.noreply.github.com>
2023-01-05 18:23:05 +0000

    final

---
+++ js/search.js
@@ -0,0 +1,27 @@
+// search web
+
+
+// take search
+function go() {
+    let request = document.getElementById('search').value;
+
+    if (request.charAt(0) == '!') window.location.href = 'https://' + request.replace('!','');
+    else                          window.location.href = `http://www.google.com/search?q=${request}`;
+}
+
+// clear search
+function clear_search() {
+    document.getElementById('search').value = '';
+}
+
+// detect key input
+$("#search").keyup(function(event) {
+    // enter
+    if (event.keyCode === 13) {
+        $("#go").click();
+    }
+    // escape
+    if (event.keyCode === 27) {
+        clear_search();
+    }
+});
