6d1aba83583c52abd01a9856d8798b0d7ea3d689
plexion <46572320+plexiondev@users.noreply.github.com>
2023-01-05 17:58:29 +0000

    time of day

--- js/datetime.js
+++ js/datetime.js
@@ -1,12 +1,22 @@
 // date & time
 
 
+let time_of_day = 'morning';
+
 function update_date_time() {
-    let date_now = new Date(Date.now());
+    let date_now = new Date();
     let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
     document.getElementById('time').innerHTML = date_now.toLocaleTimeString();
     document.getElementById('date').textContent = `${months[date_now.getMonth()]} ${pad_num(date_now.getDate())} ${date_now.getFullYear()}`;
+
+    // time of day
+    let date_hours = date_now.getHours();
+    if      (date_hours < 12) time_of_day = 'morning';
+    else if (date_hours < 18) time_of_day = 'afternoon';
+    else                      time_of_day = 'evening';
+
+    document.body.setAttribute('time-of-day',time_of_day);
 }
 
 // pad number
