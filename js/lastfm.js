bf93830a97620204eab3d518d02227c4bd358095
plexion <46572320+plexiondev@users.noreply.github.com>
2023-02-16 22:08:33 +0000

    it actually works lmao

--- js/lastfm.js
+++ js/lastfm.js
@@ -3,11 +3,14 @@
 
 let api_key;
 let use_api = false;
+console.log('lastfm');
 
 // api keys
 function get_api_key() {
     api_key = localStorage.getItem('lastfm_api_key') || '';
     if (api_key != '') use_api = true;
+
+    console.log('using api?',use_api);
 }
 
 function set_api_key(key) {
@@ -18,6 +21,54 @@ function set_api_key(key) {
 
 // get tracks
 function get_tracks() {
-    let xhr = new XMLHttpRequest();
-    let url = 'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=plexion4k&format=json&limit=10&period=7day&api_key=' + api_key;
+    let api_expire = localStorage.getItem('lastfm_api_expire') || '';
+    if (new Date() > new Date(api_expire) || api_expire == '')
+        get_tracks_new();
+    else
+        get_tracks_cache();
 }
+
+function get_tracks_new() {
+    let xhr = new XMLHttpRequest();
+    let url = 'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=plexion4k&format=json&limit=1&api_key=' + api_key;
+    xhr.open('GET',url,true);
+
+    xhr.onload = function() {
+        load_tracks(JSON.parse(this.response));
+
+        let api_expire = new Date();
+        api_expire.getSeconds(api_expire.getSeconds() + 10);
+        localStorage.setItem('lastfm_api_expire',api_expire);
+        localStorage.setItem('lastfm_api_data',this.response);
+    }
+
+    xhr.send();
+}
+
+function get_tracks_cache() {
+    let api_data = localStorage.getItem('lastfm_api_cache') || '';
+    if (api_data != '')
+        load_tracks(JSON.parse(api_data));
+    else
+        get_tracks_new();
+}
+
+function load_tracks(data) {
+    let tracks = data.recenttracks.track;
+    console.log('loaded tracks!');
+
+    document.getElementById('track').textContent = tracks[0].name;
+    document.getElementById('artist').textContent = tracks[0].artist['#text'];
+}
+
+
+// loop
+setTimeout(function() {
+    //console.log('loaded');
+    get_api_key();
+
+    if (use_api) {
+        get_tracks();
+        setInterval(get_tracks,3000);
+    }
+},100);
